8ad2863987337002ee37e2c4a3ff448e58997f73
Woo Ji Yoon <wootekken@naver.com>
2020-10-05 00:49:06 +0900

    PainJS_Final
    PaintJS Final Code.
    It ended at 2020-10-05

---
+++ app.js
@@ -0,0 +1,109 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "black";
+const INITIAL_LINE_WIDTH = 2.5;
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 400;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = INITIAL_LINE_WIDTH;
+
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+    painting = false;
+}
+
+function startPainting() {
+    painting = true;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+    const size = event.target.value; // line size
+    ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+    if(filling === true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
+
+function handleCanvasClick() {
+    if(filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+function handleCM(event) { // CM = contextmenu
+    event.preventDefault(); // prevent mouse right click
+}
+
+function handleSaveClick() {
+    //const image = canvas.toDataURL("image/jpeg");
+    const image = canvas.toDataURL(); // png as default
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click(); // automatic click to save a image filefrom image link
+}
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM); // mouse right click event
+}
+
+//console.log(Array.from(colors)); // make a object into array
+Array.from(colors).forEach(myColor => myColor.addEventListener("click",handleColorClick));
+
+if(range) {
+    range.addEventListener("input", handleRangeChange);
+}
+
+if(mode) {
+    mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
+}
